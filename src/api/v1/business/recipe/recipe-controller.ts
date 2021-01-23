9f04c6f1c564152349a28f7eb466a345664b06b4
Daniel Bonfim <daniel.bonfim@aisdigital.com.br>
2021-01-23 16:08:11 -0300

    feat: get recipe function

--- src/api/v1/business/recipe/recipe-controller.ts
+++ src/api/v1/business/recipe/recipe-controller.ts
@@ -1,8 +1,17 @@
 import { NextFunction, Request, Response } from 'express'
 import * as HTTPStatus from 'http-status'
 import BaseController from '../../../../base/base-controller'
+import { RecipePuppyIntegration } from '../../../../integrations/recipe-puppy/recipe-puppy-integration'
+import { IRecipeResponse } from './recipe-types'
 
 export default class RecipeController extends BaseController {
+  protected recipePuppyIntegration: RecipePuppyIntegration
+
+  constructor() {
+    super()
+    this.recipePuppyIntegration = new RecipePuppyIntegration()
+  }
+
   /**
    * Get Recipe
    *
@@ -16,7 +25,26 @@ export default class RecipeController extends BaseController {
     try {
       this.checkValidationErrors(req)
 
-      res.status(HTTPStatus.OK).send(':)')
+      const ingredients = req.query.i as string
+
+      const recipePuppyResponse = await this.recipePuppyIntegration.getRecipes(ingredients)
+
+      const response: IRecipeResponse = {
+        keywords: ingredients.split(','),
+        recipes: recipePuppyResponse.results.map(result => {
+          return {
+            gif: result.thumbnail,
+            link: result.href,
+            title: result.title,
+            ingredients: result.ingredients
+              .split(',')
+              .map(ingredient => ingredient.trim())
+              .sort((a, b) => a.localeCompare(b)),
+          }
+        }),
+      }
+
+      res.status(HTTPStatus.OK).json(response)
     } catch (err) {
       next(err)
     }
